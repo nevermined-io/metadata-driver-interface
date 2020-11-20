6d7a3e2a9adf4ae508dbadf42d8688d786317292
enrique <enrique@keyko.io>
2020-11-20 13:49:14 +0100

    Modify test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,7 +6,7 @@ from metadata_driver_interface.utils import retrieve_module_path
 
 def test_retrieve_module_path():
     _type = 'data'
-    module = 'azure'
+    module = 'onprem'
     config = './tests/config.ini'
     assert retrieve_module_path(_type=_type, module=module,
                                 config=config) == f'{os.getenv("VIRTUAL_ENV")}/lib/python3.{sys.version_info[1]}' \
