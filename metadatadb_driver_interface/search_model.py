da48e7941fa14ec37dde95f1802681acbca9bd73
Ialberquilla <ivan.alberquilla@gmail.com>
2021-11-02 17:44:29 +0100

    adds text on query model

--- metadatadb_driver_interface/search_model.py
+++ metadatadb_driver_interface/search_model.py
@@ -1,9 +1,10 @@
 class QueryModel(object):
-    def __init__(self, query=None, sort: dict = None, offset=100, page=0):
+    def __init__(self, query=None, sort: dict = None, offset=100, page=0, text=None):
         self.query = query
         self.sort = sort
         self.offset = offset
         self.page = page
+        self.text = text
 
 
 class FullTextModel(object):
