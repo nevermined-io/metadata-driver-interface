9444cfdd73af27cb2c90b0d27173b85c5ad939d2
enrique <enrique@keyko.io>
2021-11-03 08:45:57 +0100

    Bump version: 0.2.0 → 0.3.0

--- setup.py
+++ setup.py
@@ -68,6 +68,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/nevermined-io/metadata-driver-interface',
-    version='0.2.0',
+    version='0.3.0',
     zip_safe=False,
 )
